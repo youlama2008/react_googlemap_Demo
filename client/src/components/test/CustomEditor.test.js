20efc67c7ca365015996f09924072360c51c3313
尤拉姆 <youlamu@LahmYOU.local>
2019-10-10 22:18:35 +1100

    update test

--- client/src/components/test/CustomEditor.test.js
+++ client/src/components/test/CustomEditor.test.js
@@ -1,19 +1,33 @@
 import React from "react";
 import CustomEditor from "./../CustomEditor";
 import { mount } from "./../../enzyme";
+import CustomButton from "../CustomButton";
+import CustomInput from "../CustomInput";
 
 describe("render CustomEditor properly", () => {
-  const wrapper = mount(<CustomEditor />);
-//   let wrapperProp;
-//   beforeEach(() => {
-//     wrapperProp = wrapper.props();
-//   });
+  const customEditor = mount(<CustomEditor />);
+  const customBtn = customEditor.find(CustomButton);
+  const customInput = customEditor.find(CustomInput);
 
   it("should render CustomEditor", () => {
-    expect(wrapper).toHaveLength(1);
+    expect(customEditor).toHaveLength(1);
   });
 
   it("should render CustomEditor with default class name", () => {
-    expect(wrapper.find(".custom-editor")).toBeDefined();
+    expect(customEditor.find(".custom-editor")).toBeDefined();
+  });
+
+  it("should render CustomBtn with correct props", () => {
+    expect(customBtn.props().disabled).toEqual(true);
+    expect(customBtn.props().text).toEqual("Edit");
+  });
+
+  it("should render CustomInput with correct props", () => {
+    expect(customInput.props().disabled).toEqual(false);
+    expect(customInput.props().type).toEqual("input");
+    expect(customInput.props().value).toEqual("");
+    expect(customInput.props().placeholder).toEqual(
+      "Please write your comments"
+    );
   });
 });
