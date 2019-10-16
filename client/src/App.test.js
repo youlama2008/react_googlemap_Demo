34b485d50eb42e85ee4ab9097ab00108900bdff2
尤拉姆 <youlamu@LahmYOU.local>
2019-10-17 01:53:48 +1100

    update test case for app

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,9 +7,9 @@ import stores from "./stores";
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-      <Provider {...stores}>
-        <App />
-      </Provider>,
+    <Provider {...stores}>
+      <App />
+    </Provider>,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
